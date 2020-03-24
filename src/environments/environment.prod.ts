4a54c48df843005cb6beee242352b59dfa0cbc1f
Salome Flores <salomeflores@MacBook-Pro-de-Salome.local>
2020-03-24 11:42:44 -0500

    cambio environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  urlSrvUsuario: 'http://172.16.72.143:9080/srvPresupuestoTi-web/rest/usuario',
-  urlSrvRol: 'http://172.16.72.143:9080/srvPresupuestoTi-web/rest/rol',
-  urlSrvAccion: 'http://172.16.72.143:9080/srvPresupuestoTi-web/rest/accion',
-  urlSrvMenu: 'http://172.16.72.143:9080/srvPresupuestoTi-web/rest/menu',
-  urlSrvCatalogo: 'http://172.16.72.143:9080/srvPresupuestoTi-web/rest/catalogo',
-  urlSrvCabeceraPresupuesto: 'http://172.16.72.143:9080/srvPresupuestoTi-web/rest/cabeceraPresupuesto',
-  urlSrvDetallePresupuesto: 'http://172.16.72.143:9080/srvPresupuestoTi-web/rest/detallePresupuesto'
+  urlSrvUsuario: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/usuario',
+  urlSrvRol: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/rol',
+  urlSrvAccion: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/accion',
+  urlSrvMenu: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/menu',
+  urlSrvCatalogo: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/catalogo',
+  urlSrvCabeceraPresupuesto: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/cabeceraPresupuesto',
+  urlSrvDetallePresupuesto: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/detallePresupuesto'
 };
