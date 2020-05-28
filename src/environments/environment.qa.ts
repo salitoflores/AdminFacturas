96bc95c752c3bada04226c4cc5349fb8db0652bd
Salome Flores <salomeflores@MacBook-Pro-de-Salome.local>
2020-05-28 15:56:37 -0500

    archivo de ambiente qa

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  urlSrvUsuario: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/usuario',
+  urlSrvRol: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/rol',
+  urlSrvAccion: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/accion',
+  urlSrvMenu: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/menu',
+  urlSrvCatalogo: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/catalogo',
+  urlSrvCabeceraPresupuesto: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/cabeceraPresupuesto',
+  urlSrvDetallePresupuesto: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/detallePresupuesto',
+  utlSrvFechasAutorizadas: 'http://172.16.72.129:9082/srvPresupuestoTi-web/rest/fechasAutorizadas'
+};
